b021d72ab6d6ee8b3ec031517db30064c28f3731
Sandeep Mansotra <sandeepsharma1998.sp@gmail.com>
2019-07-08 14:06:46 +0530

    ReactJs App

---
+++ src/components/AgeStats.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import partyPopper from '../assets/party-popper.jpg';
+
+class AgeStats extends Component{
+    timeSince(date){
+        let today = new Date().getTime();
+        let other_date = new Date(date).getTime();
+        let difference = Math.abs(today - other_date);
+
+        let days=Math.floor(difference/(1000*3600*24));
+        let years=Math.floor(days/365);
+        days -= years*365;
+        let months= Math.floor(days/31);
+        days -= months*31;
+
+
+        return `${years} years, ${months}, and ${days} days` 
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>{this.props.date}</h3>
+                <h4> Congrats On {this.timeSince(this.props.date)}!</h4>
+                <img src={partyPopper} alt="party-popper" className="party-popper" />
+            </div>
+        )
+    }
+}
+
+export default AgeStats;
